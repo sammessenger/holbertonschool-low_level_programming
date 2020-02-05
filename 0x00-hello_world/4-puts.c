035715ed80e6948a60c1bff29c4b4ad6553fcebb
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-05 20:15:57 +0000

    Modifying 4 for style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
+/**
+* main - prints a string
+*
+* Return: should be null
+*/
+
+
 #include <stdio.h>
 #include <string.h>
 
-int main () {
+int main(void)
+{
 puts("\"Programming is like building a multilinugla puzzle");
-return 0;
+return;
 }
