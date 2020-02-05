b738561225cc988910237fc79c225499471141a8
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-05 20:20:58 +0000

    Modifying q5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
 #include <stdlib.h>
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
