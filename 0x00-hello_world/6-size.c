4249d0f414a237981d1d463e453e7a531fb317dd
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-05 20:28:32 +0000

    Modding q6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,16 +9,10 @@
 
 int main(void)
 {
-int c = sizeof(char);
-int i = sizeof(int);
-int l = sizeof(long);
-int o = sizeof(long long);
-int f = sizeof(float);
-
-printf("Size of a char: %i byte(s) \n", c);
-printf("Size of an int: %i byte(s) \n", i);
-printf("Size of a long int: %i byte(s) \n", l);
-printf("Size of a long long int: %i byte(s) \n", o);
-printf("Size of a float: %i byte(s)\n", f);
+printf("Size of a char: %i byte(s) \n", sizeof(char));
+printf("Size of an int: %i byte(s) \n", sizeof(int);
+printf("Size of a long int: %i byte(s) \n", sizeof(long));
+printf("Size of a long long int: %i byte(s) \n", sizeof (long long));
+printf("Size of a float: %i byte(s)\n", sizeof (float));
 return (0);
 }
