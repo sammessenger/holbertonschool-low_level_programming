295551fc5e13ea822b960450e8b11f31bf0c3a20
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-06 21:51:16 +0000

    Adding q11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+/**
+ * main - should print 0-99
+ *
+ * Return: null
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int i;
+	int a;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (a = 48; a <= 57; a++)
+		{
+			if (a > i)
+			{
+			putchar(i);
+			putchar(a);
+				if (i != 56)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
