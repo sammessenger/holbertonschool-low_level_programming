154f92cd7016afaaf8c9ca07daeaaf44a6fdb1a3
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-06 18:04:43 +0000

    Adding q2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - should print lowercase alphabet
+ *
+ * Return: should be 0/null
+*/
+int main(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+	putchar (i);
+	putchar ('\n');
+return (0);
+}
