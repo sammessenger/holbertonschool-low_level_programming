5b448000fc2b345271dd9851022d14cb2b2d3242
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-11 21:14:33 +0000

    Adding q8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+/**
+ * jack_bauer - print out every minute of the day
+ *
+ * Return: should be 0
+*/
+#include "holberton.h"
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			if (h < 10)
+			{
+				_putchar('0');
+				_putchar(h + '0');
+			}
+			else
+			{
+				_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
+			}
+			if (m < 10)
+			{
+				_putchar('0');
+				_putchar(m + '0');
+			}
+			else
+			{
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+
+
+
+}
