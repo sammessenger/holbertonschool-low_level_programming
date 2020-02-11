7ad38dfb98c62744a91ed1e7b38ffd2043553850
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-11 18:21:23 +0000

    Adding q3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+ * _islower - should check if a letter is lowercase
+ *
+ * Return: 1 if lowercase, 0 otherwise
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
