aec6acc2bfdd688a3e19ac37aa3ac1da67cc9c87
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-12 21:22:04 +0000

    Updating header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +16,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+void positive_or_negatve(int n);
+
+#endif
