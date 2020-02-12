eea9308600c2b899b635dd3c4f08935660d37886
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-12 21:40:09 +0000

    Adding q14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+/**
+ * main - print first 50 fibbonaci
+ *
+ * Return: should be null
+*/
+#include "holberton.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int a = 1;
+	int b = 1;
+	int c = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		printf("%i, ", c);
+		a = b + c;
+		b = c;
+		c = a;
+	}
+	return (0);
+}
