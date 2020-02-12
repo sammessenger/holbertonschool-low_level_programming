10d425c3c204d69f4af90de35216783ccd777084
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-12 21:17:06 +0000

    Adding 13

---
+++ 0x03-debugging/101-natural.c
@@ -0,0 +1,22 @@
+/**
+ * main - shoould compute and print sum of all multiples of 3 or 5 below 1024
+ *
+ * Return: should be null
+*/
+#include "holberton.h"
+#include <stdlib.h>
+int main(void)
+{
+	int i;
+	int sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+		sum += i;
+		}
+	}
+	printf("%i \n", sum);
+	return (0);
+}
