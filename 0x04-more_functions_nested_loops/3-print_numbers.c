468c218c82dfb212618719d8d862c903a1d979d6
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-13 20:05:30 +0000

    Adding q3 BLIND

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+/**
+ * print_numbers - should print 1-9
+ * Return: should be void
+*/
+#include "holberton.h"
+void print_numbers(void)
+{
+	int i;
+	for(i = '0'; i <= '9'; i++)
+		_putchar(i);
+
+}
