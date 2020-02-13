c45c6d20f6f6151427bbd1a4b370bae89f0f3d26
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-13 19:29:49 +0000

    Adding q7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - should print diagonal lines
+ * @n: lines to be printed
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int i, a;
+
+	for(i = 0; i < n; i++)
+	{
+		for(a = 0; a < i; a++)
+		{
+			_putchar(' ');
+		}
+
+
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
