d6b757c00ce6750631b4ad6ac450af5f6eb955bf
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-13 17:52:24 +0000

    Adding q4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - should print 0-9 except 2 and 4
+ * Return: should be null
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
+}
