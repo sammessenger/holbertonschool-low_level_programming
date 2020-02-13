b59754cd86efcd6edee877bdc9f7358b12444101
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-13 17:10:50 +0000

    adding README.md, Holberton.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,7 @@
+/*
+ * isupper - should check for uppercase  characters
+ * @c: to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+*/
+#include "holberton.h"
+int _isupper(int c)
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project covers more functions and nested loops
