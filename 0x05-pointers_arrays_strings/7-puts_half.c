7f64074ce7b1b8543b85ed20acae95970dfab6d4
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-18 21:59:24 +0000

    Adding q7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+/**
+ * puts_half - should print second half of string
+ * @str: string to be printed
+ * Return: void
+*/
+#include "holberton.h"
+void puts_half(char *str)
+{
+	int i, a;
+
+	i = 0;
+	a = 0;
+
+	while (str[i])
+	i++;
+
+	if (a % 2 == 0)
+	a = i / 2;
+	else
+	a = (i / 2) + 1;
+
+	for (; a < i; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
