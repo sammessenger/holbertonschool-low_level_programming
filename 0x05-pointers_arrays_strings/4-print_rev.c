42bf06dd6ba6339431439611883befddcff45f15
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-18 18:04:12 +0000

    Adding q4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+/**
+ * print_rev - should print a string in reverse
+ * @s: string to be printed
+ * Return: should be null
+*/
+#include "holberton.h"
+void print_rev(char *s)
+{
+	int i;
+
+	while(s[i])
+	{
+		i++;
+	}
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
