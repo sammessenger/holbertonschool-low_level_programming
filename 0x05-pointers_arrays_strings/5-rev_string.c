64f81cace7936ba62324fd5def1b499e8f1243c5
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-18 21:40:02 +0000

    Adding q5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+/**
+ * rev_string - should reverse a string
+ * @s: string to be reversed
+ * Return: void
+*/
+#include "holberton.h"
+#include <stdio.h>
+
+void rev_string(char *s)
+{
+	int i, a;
+	char *rev;
+
+	a = 0;
+	i = 0;
+
+	while (s[i])
+	i++;
+
+	rev = s + i + 1;
+
+	for (a = 0; a < i; a++)
+	{
+		rev[a] = s[a];
+	}
+	for (a = 0; i > 0; i--, a++)
+	{
+		s[a] = rev[i - 1];
+	}
+}
