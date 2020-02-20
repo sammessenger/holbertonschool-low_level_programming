8f80b98c55d86660762d049115ed16c5047d85e6
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-20 22:57:21 +0000

    Adding q13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+/**
+ * rot13 - should convert a string to rot13 
+ * @s: pointer to first element of string
+ * Return: should be pointer to first element of string
+*/
+char *rot13(char *s)
+{
+	char arr[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\0";
+	char com[53] = "nopqrstuvwxyzabcdefghijklmNOPQRSTYVWXYZABCDEFGHIJKLM\0";
+	int i = 0;
+	int a = 0;
+
+	char *rot = s;
+
+	while (rot[i])
+	{
+		while (arr[a])
+		{
+			if (s[i] == arr[a])
+			{
+				rot[i] = com[a];
+			}
+			a++;
+		}
+		i++;
+		a = 0;
+	}
+	return (rot);
+
+}
