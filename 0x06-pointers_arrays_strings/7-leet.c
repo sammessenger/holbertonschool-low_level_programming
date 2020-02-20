ade25d7e7d4e7e690127b7ae67596ea13629dc19
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-20 22:45:46 +0000

    Adding q7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+/**
+ * leet - should convert a string to 1337 speak
+ * @c: pointer to first element of string
+ * Return: should be pointer to first element of string
+*/
+char *leet (char *s)
+{
+	char arr[11] = "aAeEoOtTlL\0";
+	char com[11] = "4433007711\0";
+	int i = 0;
+	int a = 0;
+
+	while (s[i])
+	{
+		while (arr[a])
+		{
+			if (s[i] == arr[a])
+			{
+				s[i] = com[a];
+			}
+			a++;
+		}
+		i++;
+		a = 0;
+	}
+	return (s);
+
+}
