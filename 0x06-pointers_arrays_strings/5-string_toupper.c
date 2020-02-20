9f6baeb65ad7967355ef3ccbc13d7afaf18d3377
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-20 21:38:28 +0000

    Adding q5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i])
+	{
+		if (c[i] >= 97 && c[i] <= 122)
+		{
+			c[i] -= 32;
+		}
+		i++;
+	}
+	return (c);
+
+
+}
