69026ddd4b6d560adc7c2800a70b4ae98e5ee936
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-24 18:26:12 +0000

    Adding a3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+/**
+ * _strspn - gets length of prefix substring
+ * @s: pointer to substring
+ * @accept: bytes to accpet
+ * Return: bytes which consist of accpet from s
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int scount = 0;
+	int acount = 0;
+	int i;
+	int a;
+	int ret = 0;
+
+	while (s[scount])
+	scount++;
+
+	while (accept[acount])
+	acount++;
+
+
+	for (i = 0; i < scount; i++)
+	{
+		for (a = 0; a < acount; a++)
+		{
+			while (s[i] == accept[a])
+			{
+			ret++;
+			i++;
+			}
+		}
+	}
+	return (ret);
+}
