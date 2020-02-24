2c44ff0643631dbd6dd1245d6941ed3f180e3280
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-24 22:03:37 +0000

    Adding q9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+/**
+ * set_string - should set value of a pointer to a char
+ * @s: pointer to the pointer we are changing
+ * @to: char to set the pointer to
+ * Return: void
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
