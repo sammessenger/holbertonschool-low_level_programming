00cead9f3dcd5ed522dc1f0c5a99b0c0bfdb96a4
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-24 18:09:02 +0000

    Adding q2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - locate a character in a string
+ * @s: string to check
+ * @c: char to check for
+ * Return: pointer to first occurrence of c in s, or NULL if none is found
+*/
+#include <stddef.h>
+char *_strchr(char *s, char c)
+{
+	while (s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return (NULL);
+}
