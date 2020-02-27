6dae22579c180527eb1c7a9ffa630e86969d6517
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-27 04:14:46 +0000

    Adding q6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+/**
+ * prime_checker - recuursively checks if a number is prime
+ * @i: recursive iterator
+ * @n: number to ccheck
+ * Return: 1 if prime
+*/
+int prime_checker(int n, int i)
+{
+	if (n == 1 || n == -1)
+	{
+		return (0);
+	}
+
+
+	if (n == 2)
+	return (1);
+
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	if (i * i > n)
+	{
+		return (1);
+	}
+	return (prime_checker(n, i + 1));
+}
+/**
+ * is_prime_number - should determine if number is rpime
+ * @n: number to check
+ * Return: 1 if prime
+*/
+int is_prime_number(int n)
+{
+
+	return (prime_checker(n, 2));
+}
