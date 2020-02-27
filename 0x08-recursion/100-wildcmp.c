f01268d4d4a09d034337ebb89124df2619e2b113
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-27 21:23:24 +0000

    Adding q8

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,26 @@
+/**
+ * wildcmp - should compare two strings with wildcards
+ * @s1: first string
+ * @s2: second string
+ * Return: should be 1 if identical
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if(*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if(*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	{
+		return (0);
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	return (0);
+}
