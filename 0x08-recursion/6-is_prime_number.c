7eda318f8eead8aa5a2104017a3eac2bdca851de
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-27 05:21:42 +0000

    Rewrote code for edge case

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,24 +6,20 @@
 */
 int prime_checker(int n, int i)
 {
-	if (n == 0)
+	if (n == 0 || n == 1)
 	return (0);
 
-	if (n == 1)
-	return (0);
-
-	if (n == 2)
+	if (i == 1)
 	return (1);
 
-	if (n % i == 0)
+	else
 	{
+		if (n % i == 0)
 		return (0);
+
+		else
+		return (prime_checker(n, i - 1));
 	}
-	if (i * i > n)
-	{
-		return (1);
-	}
-	return (prime_checker(n, i + 1));
 }
 /**
  * is_prime_number - should determine if number is rpime
@@ -36,5 +32,5 @@ int is_prime_number(int n)
 	n = -n;
 
 
-	return (prime_checker(n, 2));
+	return (prime_checker(n, n / 2));
 }
