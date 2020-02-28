cc9e1318d315d22b91d5df3e08bc35d243bcac23
Sam Messenger <sam.messenger1997@gmail.com>
2020-02-28 21:16:11 +0000

    Adding subdir, Readme, header, q0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc - count of arguments
+ * @argv - arguments
+ * Return: should be 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc-argc]);
+
+	return (0);
+}
