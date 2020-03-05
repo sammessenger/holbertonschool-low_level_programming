1fb6dfca4c2ef0fd0345ec2b1eb34c75c0eba101
Sam Messenger <sam.messenger1997@gmail.com>
2020-03-05 23:17:18 +0000

    Modding q1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
  * @s1: first string
  * @s2: second string
  * @n: number of bytes from s2 to append to s1
- * @return: a pointer to the new string
+ * Return: a pointer to the new string
 */
 #include "holberton.h"
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -12,15 +12,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int b = 0;
 	char *p;
 
+
+	if(!s1)
+	{
+		s1 = "";
+	}
+	if (!s2)
+	{
+		s2 = "";
+	}
+
 	while (s1[i])
 	i++;
 
-	p = malloc((i + n) * sizeof *s1);
+	p = malloc(((i + n) * sizeof(*s1)) + 1);
 
 	if (!p)
 	return (NULL);
 
-	for (a = 0; a <= i; a++)
+	for (a = 0; a < i; a++)
 	{
 		p[a] = s1[a];
 	}
@@ -29,6 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[a] = s2[b];
 	}
+	p[a] = '\0';
 	return (p);
 
 }
