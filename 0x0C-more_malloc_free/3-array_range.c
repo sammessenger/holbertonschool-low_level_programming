044608cb95511d0cbbb7a43844f990b0cfcc6f08
Sam Messenger <sam.messenger1997@gmail.com>
2020-03-06 01:54:35 +0000

    Adding q3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * array_range - should create an array from min to max
+ * @min: first number
+ * @max: second number
+ * Return: should be pointer to the array
+*/
+int *array_range(int min, int max)
+{
+	int *p;
+	int i;
+	int a = 0;
+
+	if (min > max)
+		return (NULL);
+
+	i = max - min;
+
+	p = malloc(sizeof(int) * i);
+
+	for (i = min; i <= max; i++, a++)
+	{
+		p[a] = i;
+	}
+	return (p);
+}
