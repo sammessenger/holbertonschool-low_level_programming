39c12879dc0002e76894ed504aa7e2d8fde52653
Sam Messenger <sam.messenger1997@gmail.com>
2020-03-11 23:22:53 +0000

    Adding q1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - iterates through array performs an action on each element
+ * @array: array to iterate through
+ * @size: size of array
+ * @action: function for the aciton to perform
+ * return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array)
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+
+
+
+}
