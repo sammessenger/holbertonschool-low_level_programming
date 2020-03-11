0f7e4490325db7afa5744e1fe7f640fd7a63e4e8
Sam Messenger <sam.messenger1997@gmail.com>
2020-03-11 22:13:50 +0000

    Modding for edge case and betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
+/**
+ * print_name - should print a name
+ * @name: name to be printed
+ * @f: function to pick
+ * Return: void
+*/
 void print_name(char *name, void (*f)(char *c))
 {
+	if (name && f);
 	f(name);
 }
